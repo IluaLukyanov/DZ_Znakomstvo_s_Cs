1db23d0b73003b7aac0a8338369fa3b73c28d497
Ilya Lukyanov <lis11011999@yandex.ru>
2022-11-03 01:06:36 +0300

    Z21

---
+++ Z21/Program.cs
@@ -0,0 +1,15 @@
+﻿double[] massA = new double[3];
+double[] massB = new double[3];
+double[] AB = new double[3];
+double rast = 0;
+int i;
+Console.WriteLine("Введите координаты A:");
+for (i=0; i<3; i++) massA[i] = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты B:");
+for (i=0; i<3; i++) massB[i] = Convert.ToDouble(Console.ReadLine());
+for (i=0; i<3; i++) 
+{
+    AB[i] = massA[i]-massB[i];
+    rast += AB[i]*AB[i];
+}
+Console.WriteLine("Растояние между точками А и В равно {0}", Math.Sqrt(rast));
