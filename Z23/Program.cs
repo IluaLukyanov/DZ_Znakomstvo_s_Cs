1b7a60a22ae39da76994a3af619d45091d65df0f
Ilya Lukyanov <lis11011999@yandex.ru>
2022-11-03 00:19:40 +0300

    Z23

---
+++ Z23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число для нахождения ряда кубов чисел до него начиная с 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[a];
+int i;
+Console.WriteLine("Четные числа до введенного числа:");
+for (i=0; i<a; i++)
+{
+    mass[i] = (i+1)*(i+1)*(i+1);
+    Console.WriteLine("{0} {1}", i+1, mass[i]);
+}
