2f34a741e8d4857d142d8638cc1bdb4942df7cde
Ilya Lukyanov <lis11011999@yandex.ru>
2022-11-03 17:10:24 +0300

    Z29

---
+++ Z29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите длинну массмва N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[N];
+int i;
+Console.WriteLine("Введите числа:");
+for (i=0; i<mass.Length;i++) mass[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введенные числа:");
+for (i=0; i<mass.Length;i++) Console.Write("{0} ", mass[i]);
+
+
