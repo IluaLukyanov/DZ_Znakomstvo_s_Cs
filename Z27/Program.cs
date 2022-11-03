f711a6160f81fb022a967bf6793249b6b93d3a2c
Ilya Lukyanov <lis11011999@yandex.ru>
2022-11-03 16:29:31 +0300

    Z27

---
+++ Z27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число, для которого необходимо найти сумму его цифр: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b =0, c, v = a;
+while (v>0)
+{
+    c = v % 10;
+    b = b + c;
+    v = v/10;
+}
+Console.Write("Сумма цифр числа {0} равна {1}", a, b);
